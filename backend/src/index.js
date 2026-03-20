8497e6bc17eaa4ac7a4cba3db356d1eb01968502
Abhimanyu kumar <166692284+abhi-webdev@users.noreply.github.com>
2026-03-20 09:35:59 +0530

    Problem boiler plate ready

--- backend/src/index.js
+++ backend/src/index.js
@@ -3,6 +3,7 @@ import dotenv from "dotenv";
 import cookieParser from "cookie-parser";
 
 import authRoutes from './routes/auth.routes.js';
+import problemRoutes from './routes/problem.routes.js';
 
 dotenv.config();
 const app = express();
@@ -12,6 +13,7 @@ app.use(express.json());
 app.use(cookieParser());
 
 app.use("/api/v1/auth", authRoutes);
+app.use("/api/v1/problems", problemRoutes);
 
 app.get("/", (req, res) => {
     res.send("Hello gyus, welcome to leetlab");
--- backend/src/middleware/auth.middleware.js
+++ backend/src/middleware/auth.middleware.js
@@ -42,3 +42,25 @@ export const authMiddleware = async (req, res, next) => {
     }
 }
 
+export const checkAdmin = async(req, res, next) => {
+    try {
+        const userId = req.user.id;
+        const user = await db.user.findUnique({
+            where : {
+                id : userId
+            },
+            select : {
+                role : true
+            }
+        })
+
+        if(!user && user.role !== "ADMIN"){
+            return res.status(403).json({ msg: "Unauthorized - User is not admin" });
+        }
+        next();
+    } catch (error) {
+        console.log("Error check admin", error);
+
+        res.status(400).json({ error: "Error check admin", msg: error.message });
+    }
+}
